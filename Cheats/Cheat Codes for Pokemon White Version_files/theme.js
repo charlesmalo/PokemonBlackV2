92a1c156b8b0244741b18852d25e5498775cbbd7
Charles Malo <charles.malo@gmail.com>
2015-01-15 06:49:05 -0500

    first commit without the ROMs

---
+++ Cheats/Cheat Codes for Pokemon White Version_files/theme.js
@@ -0,0 +1,32 @@
+﻿var count = 0;
+function Anthem_PreCallBack() { 
+    count++;
+    setTimeout("CallBackStarted()", 500); 
+}
+function Anthem_PostCallBack() { CallBackFinished(); }
+
+function CallBackStarted() {
+  if (count > 0){  
+    var item = document.getElementById('wait');
+    if ((!window.XMLHttpRequest) && (document.documentElement)){
+      item.style.position = 'absolute';
+      //code to make ie6 behave
+      if (document.documentElement.scrollTop){
+        item.style.top = document.documentElement.scrollTop + 15;
+      }
+      else{ item.style.top = 15; }      
+      item.style.right = 15;
+    }
+    item.style.display = 'block';    
+  }
+}
+ 
+function CallBackFinished(){
+  count--;
+  if (count == 0){
+    var item = document.getElementById('wait');
+    if (item != null){
+        item.style.display = 'none';
+    }
+  }
+}
